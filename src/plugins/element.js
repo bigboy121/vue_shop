69dccf550117e18be2bfa590708a8ae66ae2e9c9
apple <apple@appledeMacBook-Pro.local>
2020-04-16 09:48:54 +0800

    完成登陆功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,12 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
+
+
+// 全局注册使用
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 导入弹框提示 需要全局挂载
+Vue.prototype.$message = Message
