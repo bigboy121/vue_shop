74db0fd643751219db10e508a673ea136db70a36
apple <apple@appledeMacBook-Pro.local>
2020-04-18 11:18:08 +0800

    完成用户列表功能的开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,167 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/logo.png" alt />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ? '64px' : '200px' ">
+        <div class="toggle-button-l" v-if="!isCollapse" @click="toggleCollapse">《《《</div>
+        <div class="toggle-button-r" v-else @click="toggleCollapse">》》》</div>
+        <!-- 侧边栏菜单区 -->
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409EFF"
+          :unique-opened="true"
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          :router="true"
+          :default-active="activePath"
+        >
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id + ''" v-for="item in menuList" :key="item.id">
+            <!-- 一级菜单的模板区 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconsObj[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{item.authName}}</span>
+            </template>
+
+            <!-- 二级菜单 -->
+            <el-menu-item
+              :index="'/' + subItem.path + ''"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+              @click="saveNavState('/' + subItem.path)"
+            >
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-menu"></i>
+                <!-- 文本 -->
+                <span>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>
+        <!-- 路由占位符 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-  name: 'Home',
+  name: "Home",
+  data() {
+    return {
+      // 左侧菜单数据
+      menuList: [],
+      iconsObj: {
+        "125": "iconfont icon-user",
+        "103": "iconfont icon-tijikongjian",
+        "101": "iconfont icon-shangpin",
+        "102": "iconfont icon-danju",
+        "145": "iconfont icon-baobiao"
+      },
+      // 控制菜单是否折叠
+      isCollapse: false,
+      // 被激活的连接地址
+      activePath: ""
+    };
+  },
+  created() {
+    this.getMenuList();
+    this.activePath = window.sessionStorage.getItem("activePath")
+  },
   methods: {
     logout() {
-      window.sessionStorage.clear()
-      this.$router.push('/login')
+      window.sessionStorage.clear();
+      this.$router.push("/login");
+    },
+    // 获取所有的菜单
+    async getMenuList() {
+      const { data: res } = await this.$http.get("menus");
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg);
+      this.menuList = res.data;
+    },
+    // 点击按钮 切换菜单的折叠展开效果
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse;
+    },
+    // 保存连接的激活
+    saveNavState(activePath) {
+      window.sessionStorage.setItem("activePath", activePath);
+      this.activePath = activePath
     }
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  display: flex;
+  justify-content: space-between;
+  background-color: #373d41;
+  padding-left: 10px;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+.el-header img {
+  border-radius: 50%;
+  width: 50px;
+  box-shadow: -1px -1px 2px #eaedf1;
+}
+.el-aside {
+  transition: 0.4s;
+  background-color: #333744;
+  .el-menu {
+    border-right: none;
+  }
+}
+.el-main {
+  background-color: #eaedf1;
+}
+.iconfont {
+  margin-right: 10px;
+}
+
+.toggle-button-l,
+.toggle-button-r {
+  text-align: center;
+  background-color: #4a5064;
+  color: #fff;
+  font-size: 14px;
+  padding: 3px 0;
+  letter-spacing: -0.5em;
+  font-weight: 100;
+  cursor: pointer;
+}
+.toggle-button-l:hover,
+.toggle-button-r:hover {
+  transition: 0.2s;
+  background-color: #4a8064;
+}
 </style>
